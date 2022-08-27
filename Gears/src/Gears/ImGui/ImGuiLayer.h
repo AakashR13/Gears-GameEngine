0f86688d2ce087085a9a3dfe1bb18c60078c5593
AakashR13 <Aakash.re13@gmail.com>
2022-08-28 05:18:54 +0530

    ImGui Events
    Now able to interact within the window
    Need to implement copy paste

--- Gears/src/Gears/ImGui/ImGuiLayer.h
+++ Gears/src/Gears/ImGui/ImGuiLayer.h
@@ -2,6 +2,11 @@
 
 #include "Gears/Layer.h"
 
+#include "Gears/Events/ApplicationEvent.h"
+#include "Gears/Events/KeyEvent.h"
+#include "Gears/Events/MouseEvent.h"
+
+
 namespace Gears {
 	
 	class GEARS_API ImGuiLayer : public Layer
@@ -14,6 +19,16 @@ namespace Gears {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
+
 	private:
 		float m_Time = 0.0f;
 	};
